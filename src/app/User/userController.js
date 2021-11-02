3478b7d8215ec0df62a2973b843728b699871597
dldydtjs2965 <54692225+dldydtjs2965@users.noreply.github.com>
2021-11-02 16:24:39 +0900

    [#5] Feat: userLogin update

--- src/app/User/userController.js
+++ src/app/User/userController.js
@@ -13,7 +13,7 @@ const {emit} = require("nodemon");
  * [GET] /app/test
  */
 exports.getTest = async function (req, res) {
-     return res.send(response(baseResponse.SUCCESS))
+     return res.send(response(baseResponse.SUCCESS("성공 입니다")))
 }
 
 /**
@@ -53,100 +53,28 @@ exports.postUsers = async function (req, res) {
 };
 
 /**
- * API No. 2
- * API Name : 유저 조회 API (+ 이메일로 검색 조회)
- * [GET] /app/users
+ * API Name : 유저 로그인 API
+ * [POST] /inflearn/users/login
  */
-exports.getUsers = async function (req, res) {
+exports.postLoginUsers = async function (req, res) {
 
     /**
      * Query String: email
      */
-    const email = req.query.email;
+    const {userId, password} = req.body;
+    // 유저 아이디 값 확인
+    if (!userId) return res.send(errResponse(baseResponse.USER_ID_EMPTY));
+    // 유저 패스워드 값 확인
+    if (!password) return res.send(errResponse(baseResponse.USER_PASSWORD_EMPTY));
+    // 유저아이디 길이 확인
+    if(userId.length>50) return res.send(errResponse(baseResponse.SIGNUP_USER_ID_LENGTH));
+    // 유저아이디 형식 확인
+    if(!regexEmail.test(userId)) return res.send(errResponse(baseResponse.SIGNUP_USER_ID_ERROR_TYPE));
 
-    if (!email) {
-        // 유저 전체 조회
-        const userListResult = await userProvider.retrieveUserList();
-        return res.send(response(baseResponse.SUCCESS, userListResult));
-    } else {
-        // 유저 검색 조회
-        const userListByEmail = await userProvider.retrieveUserList(email);
-        return res.send(response(baseResponse.SUCCESS, userListByEmail));
-    }
+    const userLoginResult = await userService.loginUser(userId,password);
+
+    return res.send(userLoginResult);
 };
-
-/**
- * API No. 3
- * API Name : 특정 유저 조회 API
- * [GET] /app/users/{userId}
- */
-exports.getUserById = async function (req, res) {
-
-    /**
-     * Path Variable: userId
-     */
-    const userId = req.params.userId;
-
-    if (!userId) return res.send(errResponse(baseResponse.USER_USERID_EMPTY));
-
-    const userByUserId = await userProvider.retrieveUser(userId);
-    return res.send(response(baseResponse.SUCCESS, userByUserId));
-};
-
-
-// TODO: After 로그인 인증 방법 (JWT)
-/**
- * API No. 4
- * API Name : 로그인 API
- * [POST] /app/login
- * body : email, passsword
- */
-exports.login = async function (req, res) {
-
-    const {email, password} = req.body;
-
-    // TODO: email, password 형식적 Validation
-
-    const signInResponse = await userService.postSignIn(email, password);
-
-    return res.send(signInResponse);
-};
-
-
-/**
- * API No. 5
- * API Name : 회원 정보 수정 API + JWT + Validation
- * [PATCH] /app/users/:userId
- * path variable : userId
- * body : nickname
- */
-exports.patchUsers = async function (req, res) {
-
-    // jwt - userId, path variable :userId
-
-    const userIdFromJWT = req.verifiedToken.userId
-
-    const userId = req.params.userId;
-    const nickname = req.body.nickname;
-
-    if (userIdFromJWT != userId) {
-        res.send(errResponse(baseResponse.USER_ID_NOT_MATCH));
-    } else {
-        if (!nickname) return res.send(errResponse(baseResponse.USER_NICKNAME_EMPTY));
-
-        const editUserInfo = await userService.editUser(userId, nickname)
-        return res.send(editUserInfo);
-    }
-};
-
-
-
-
-
-
-
-
-
 
 
 /** JWT 토큰 검증 API
